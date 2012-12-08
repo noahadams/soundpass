3e3e7c43eb6317fd9db57deeabadf7aabe536033
Ryan J Daw <ryan@mobify.me>
2012-12-08 14:31:42 -0800

    Adds signup view

--- soundauth/core/urls.py
+++ soundauth/core/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 
+from soundauth.core.views import SignupView
+
 
 urlpatterns = patterns('',
     url(r'^/?$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}, name='home'),
     url(r'^login/?$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
     url(r'^logout/?$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}, name='logout'),
+    url(r'^signup/?$', SignupView.as_view(), name='logout'),
 
 )
 
