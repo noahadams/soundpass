f570880fdd14d8264b40ae10c1b396c6dfe4b8b5
Ryan J Daw <ryan@mobify.me>
2012-12-08 14:31:42 -0800

    Adds signup view

--- soundauth/core/views.py
+++ soundauth/core/views.py
@@ -1,1 +1,32 @@
-# Create your views here.
+from django.views.generic import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django.contrib.auth import login, get_backends
+
+
+
+class SignupView(FormView):
+    form_class = UserCreationForm
+    success_url = '/'
+    template_name = 'signup.html'
+
+    def form_valid(self, form):
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+
+        user = User.objects.create_user(username, '', password)
+        user.save()
+
+        backend = get_backends()[0]
+        user.backend = '%s.%s' % (backend.__module__, backend.__class__.__name__)
+        
+        login(self.request, user)
+        return super(SignupView, self).form_valid(form)
+
+
+
+
+
+
+
