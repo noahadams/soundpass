c32db6566a747ed79a2cefbfb30e571d518c706e
Ryan J Daw <ryan@mobify.me>
2012-12-08 12:53:18 -0800

    Adds login/out views

--- soundauth/core/urls.py
+++ soundauth/core/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, include, url
 
+
 urlpatterns = patterns('',
-    url(r'^/?$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    url(r'^/?$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}, name='home'),
+    url(r'^login/?$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/?$', 'django.contrib.auth.views.logout', {'template_name': 'logout.html'}, name='logout'),
+
 )
+
