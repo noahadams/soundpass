72d35338cb1ad03a556af035c309a3c58d08e9bc
Ryan J Daw <ryan@mobify.me>
2012-12-08 15:07:58 -0800

    Make sequencer replace login

--- soundauth/core/static/js/main.js
+++ soundauth/core/static/js/main.js
@@ -1,1 +1,14 @@
+$(function() {
+    var $submit = $('#id_submit');
+    var $password = $('.password_field');
 
+    
+    if ($submit.length && $password.length) {
+        if ($submit) {
+            $submit.on('click', function() {
+                $password.val(patternToPassword());
+            });
+        }
+    }
+
+});
