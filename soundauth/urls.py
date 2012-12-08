657fb479712d1e075530fef06a0970a73231c52f
Ryan J Daw <ryan@mobify.me>
2012-12-08 13:28:36 -0800

    Adds a few pages and bootstrap dark theme

--- soundauth/urls.py
+++ soundauth/urls.py
@@ -3,9 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = patterns('',
     url(r'^', include('soundauth.core.urls')),
 
     # Admin
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
