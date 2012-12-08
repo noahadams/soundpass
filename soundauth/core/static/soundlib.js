969313a85c09497e1351432affea04d0ff7b9389
Noah Adams <noah.adams@gmail.com>
2012-12-08 13:17:22 -0800

    a barebones webkit web audio api

---
+++ soundauth/core/static/soundlib.js
@@ -0,0 +1,46 @@
+/*
+ * A stupidly small webkit audio wrapper, depends on jquery
+ */
+
+(function() {
+soundLib || soundLib = {};
+
+/*
+ * init - call once to initilize the audio Context so we can play audio
+ */
+var init = soundLib.init = function() {
+    soundLib.audioContext = new webkitAudioContext();
+}
+
+/*
+ * Invoke callback(buffer) after having loaded an audio file from url
+ */
+var loadUrl = soundLib.loadUrl = function (err, cb, url) {
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.responseType = 'arraybuffer';
+
+    req.onload = function() {
+        soundLib.audioContext.decodeAudioData(req.response, callback, err);
+    }
+    req.send();
+}
+
+/*
+ * Play the buffer in our context at time
+ */
+
+var playAtTime = soundLib.playAtTime = function(buffer, time) {
+    var context = soundLib.audioContext
+      , source = context.createBufferSource();
+
+    source.buffer = buffer;                    // tell the source which sound to play
+    source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+    source.noteOn(time); // play at Time
+}
+
+soundLib.playNow = function(buffer) {
+    playAtTime(buffer, 0);
+}
+
+})();
