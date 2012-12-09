6472f5d063fdf788924c75a7cfce0baa2c4658c5
Ryan J Daw <ryan@mobify.me>
2012-12-08 18:26:27 -0800

    Next

--- soundauth/core/views.py
+++ soundauth/core/views.py
@@ -3,13 +3,23 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 from django.contrib.auth import login, get_backends
-
+from urllib import unquote
 
 
 class SignupView(FormView):
     form_class = UserCreationForm
     success_url = '/'
     template_name = 'signup.html'
+
+
+    def get_context_data(self, **context):
+        ctx = dict(context)
+        
+        if 'next' in self.request.GET:
+            ctx['next'] = unquote(self.request.GET['next'])
+
+        return ctx
+        
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
